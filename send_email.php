b283817f767c9f72b7cb4da4991240494ca15de5
Nathan Kulewoshie <kulewoshienathan@gmail.com>
2024-02-11 01:02:30 +0000

    amissah's codes

--- send_email.php
+++ send_email.php
@@ -1,24 +1,23 @@
 <?php
-echo "Hello";
-// if ($_SERVER["REQUEST_METHOD"] == "POST") {
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-//   $message = $_POST["message"];
-//   $to = $_POST["to"];
-//   $subject = $_POST["subject"];
-//   $headers = "From: $to\r\n";
-//   $headers .= "Reply-To: $to\r\n";
+  $message = $_POST["message"];
+  $to = $_POST["to"];
+  $subject = $_POST["subject"];
+  $headers = "From: $to\r\n";
+  $headers .= "Reply-To: $to\r\n";
 
   
-//   if (mail($to, $subject, $message, $headers)) {
-//     echo '<script type="text/javascript">';
-//     echo 'alert("Your message was sent!");';
-//     echo 'window.location.href = "index.html";';
-//     echo '</script>';
-//   } else {
-//     echo '<script type="text/javascript">';
-//     echo 'alert("Failed to send your message!");';
-//     echo 'window.location.href = "index.html";'; 
-//     echo '</script>';
-//   }
-// }
+  if (mail($to, $subject, $message, $headers)) {
+    echo '<script type="text/javascript">';
+    echo 'alert("Your message was sent!");';
+    echo 'window.location.href = "index.html";';
+    echo '</script>';
+  } else {
+    echo '<script type="text/javascript">';
+    echo 'alert("Failed to send your message!");';
+    echo 'window.location.href = "index.html";'; 
+    echo '</script>';
+  }
+}
 ?>
